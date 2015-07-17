1d9e80bfb70dea1240a8f88f4d039f240f944fc3
llchow <llchow@users.noreply.github.com>
2015-07-17 14:06:00 -0400

    Add more bash examples

--- note/examples/bash/hex_decimal.sh
+++ note/examples/bash/hex_decimal.sh
@@ -15,3 +15,30 @@ if ! [[ $1 =~ [A-Fa-f] ]]; then
     printf 'Hex value:\t%x\n' $1
     echo -e "Dec value:\t$1"
 fi
+
+
+cron.sh
+#!/bin/bash
+if [ "$#" -eq 0 ]; then
+    echo "Need a command"
+    exit 1
+fi
+
+sleep_duration=5
+if [ "$#" -eq 2 ]; then
+    sleep_duration=$2
+fi
+
+shopt -s expand_aliases
+
+echo -e '\nRunning command "'$1'" every "'$2'" seconds\n'
+while [ 1 ]; do
+    eval "$1"
+    sleep $sleep_duration
+done
+
+get_public_ip.sh
+#!/bin/bash
+curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+
+
